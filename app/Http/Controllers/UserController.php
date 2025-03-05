a7668e35878a10d0943ed9c0cc4f1c9b57901451
Alfonso <anillo.roalf23@cadiz.salesianos.edu>
2025-03-05 20:30:04 +0100

    Validaciones completas en UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,6 +22,11 @@ class UserController extends Controller
     $validator = Validator::make($request->all(), [
         "email" => "required|email:rfc,dns|exists:users,email",
         "password" => "required",
+    ], [
+        "email.required" => "El campo de correo electrónico es obligatorio.",
+        "email.email" => "Por favor, introduce un correo electrónico válido.",
+        "email.exists" => "El correo electrónico no está registrado.",
+        "password.required" => "El campo de contraseña es obligatorio.",
     ]);
 
     if ($validator->fails()) {
@@ -50,9 +55,18 @@ class UserController extends Controller
             "password" => "required|min:5|max:20|regex:/[a-z]/|regex:/[A-Z]/|regex:/[0-9]/",
             "password_repeat" => "required|same:password"
         ], [
-            "password.min" => "La contraseña debe contener 5 caracteres mínimo",
-            "password.max" => "La contraseña debe contener 20 caracteres máximo",
-            "password.regex" => "La contraseña debe contener una minúscula, una mayúscula y un dígito",
+            "name.required" => "El campo de nombre es obligatorio.",
+            "name.string" => "El nombre debe contener solo caracteres válidos.",
+            "name.max" => "El nombre no debe superar los 20 caracteres.",
+            "email.required" => "El campo de correo electrónico es obligatorio.",
+            "email.email" => "Por favor, introduce un correo electrónico válido.",
+            "email.unique" => "Este correo ya está registrado.",
+            "password.required" => "La contraseña es obligatoria.",
+            "password.min" => "La contraseña debe contener al menos 5 caracteres.",
+            "password.max" => "La contraseña no debe superar los 20 caracteres.",
+            "password.regex" => "La contraseña debe contener al menos una letra minúscula, una mayúscula y un dígito.",
+            "password_repeat.required" => "Debes confirmar tu contraseña.",
+            "password_repeat.same" => "Las contraseñas no coinciden.",
         ]);
 
         if ($validator->fails()) {
@@ -85,7 +99,6 @@ class UserController extends Controller
 
         return redirect()->route('login')->withErrors(['error' => 'No se pudo eliminar tu cuenta.']);
     }
-
 
 
 }
